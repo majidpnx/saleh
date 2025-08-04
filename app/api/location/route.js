4aa2465314776504e8fcecef60eeeff0e9b235bb
majid <majidv9330@gmail.com>
2025-08-04 12:37:11 +0330

    first11

--- app/api/location/route.js
+++
@@ -1,8 +0,0 @@
-export async function POST(req) {
-  const body = await req.json();
-
-  console.log("مختصات دریافت شده:", body); // { lat: ..., lng: ... }
-
-  // اینجا می‌تونی اطلاعات رو ذخیره کنی، به پایگاه داده بزنی یا هر کاری
-  return Response.json({ success: true, message: "موقعیت دریافت شد" });
-}
--- app/page.js
+++ app/page.js
@@ -4,9 +4,13 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import About from './components/About';
-import OrderForm from './components/OrderForm';
+// import OrderForm from './components/OrderForm';
 import Footer from './components/Footer';
+import dynamic from 'next/dynamic';
 
+const OrderForm = dynamic(() => import('./OrderForm'), {
+  ssr: false
+});
 
 
 export default function Home() {
